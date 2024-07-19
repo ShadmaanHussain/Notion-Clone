6f0535f4112a1b172ffa784923f0393524c92062
Shadmaan Hussain <shdhussain1299@gmail.com>
2024-07-19 17:55:13 +0530

    Sidebar sheet for mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <main className="">
       Lets build an AI Notion Clone
-      <Button>Click Me!</Button>
     </main>
   );
 }

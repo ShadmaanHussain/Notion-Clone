09e5a1c64231f3f3bcabb395901bb6e614f5be7c
Shadmaan Hussain <shdhussain1299@gmail.com>
2024-07-24 19:38:12 +0530

    Query for room data and Grouping room data using reduce function

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import NewDocumentButton from "./NewDocumentButton";
 import {
   Sheet,
@@ -9,12 +10,77 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { MenuIcon } from "lucide-react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { useUser } from "@clerk/nextjs";
+import {
+  collectionGroup,
+  DocumentData,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "@/firebase";
+
+interface RoomDocument extends DocumentData {
+  createdAt: string;
+  role: "owner" | "editor";
+  roomId: string;
+  userId: string;
+}
 
 function Sidebar() {
+  const { user } = useUser();
+  const [groupedData, setGroupedData] = useState<{
+    owner: RoomDocument[];
+    editor: RoomDocument[];
+  }>({
+    owner: [],
+    editor: [],
+  });
+
+  // Getting all the rooms a user is in
+  const [data, loading, error] = useCollection(
+    user &&
+      query(
+        collectionGroup(db, "rooms"),
+        where("userId", "==", user.emailAddresses[0].toString())
+      )
+  );
+
+  useEffect(() => {
+    if (!data) return;
+
+    const grouped = data.docs.reduce<{
+      owner: RoomDocument[];
+      editor: RoomDocument[];
+    }>(
+      (acc, curr) => {
+        const roomData = curr.data() as RoomDocument;
+
+        if (roomData.role === "owner") {
+          acc.owner.push({
+            id: curr.id,
+            ...roomData,
+          });
+        } else {
+          acc.editor.push({
+            id: curr.id,
+            ...roomData,
+          });
+        }
+
+        return acc;
+      },
+      {
+        owner: [],
+        editor: [],
+      }
+    );
+  }, [data]);
+
   const menuOptions = (
     <>
       <NewDocumentButton />
-      
+
       {/* My Documents */}
       {/* List... */}
 
@@ -39,9 +105,7 @@ function Sidebar() {
         </Sheet>
       </div>
 
-      <div className="hidden md:inline">
-        {menuOptions}
-      </div>
+      <div className="hidden md:inline">{menuOptions}</div>
     </div>
   );
 }
